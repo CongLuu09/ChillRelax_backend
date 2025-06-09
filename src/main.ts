409de38dd07e7931246da7c5153a1f195dad6fe1
Lưu Viết Công <congluu090502@gmail.com>
2025-06-09 12:43:51 +0700

    Update background Admin dashbroad

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
 import soundRoutes from './routes/sound.routes';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger.config';
 import mixRoutes from './routes/mix.routes';
 import uploadRoutes from './routes/upload.routes';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './config/swagger.config';
 
 const app = express();
 const PORT = 3000;
@@ -14,10 +14,11 @@ const PORT = 3000;
 const publicDir = path.join(__dirname, '..', 'public');
 const viewsDir = path.join(__dirname, '..', 'views');
 
+// Middleware
 app.use(express.json());
 app.use(express.static(publicDir));
 
-// API routes
+// API Routes
 app.use('/api/sounds', soundRoutes);
 app.use('/api/mix', mixRoutes);
 app.use('/api/upload', uploadRoutes);
@@ -25,22 +26,30 @@ app.use('/api/users', userRoutes);
 app.use('/api', authRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Serve HTML files from views
+
 app.get('/login', (req, res) => {
   res.sendFile(path.join(viewsDir, 'login.html'));
 });
-
-app.get('/user-dashboard', (req, res) => {
-  res.sendFile(path.join(viewsDir, 'user-dashboard.html'));
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(viewsDir, 'login.html'));
 });
+
 
 app.get('/admin', (req, res) => {
   res.sendFile(path.join(viewsDir, 'index.html'));
 });
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.join(viewsDir, 'index.html'));
+});
 
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(viewsDir, 'index.html'));
+});
 app.get('/', (req, res) => {
   res.sendFile(path.join(viewsDir, 'login.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on http://localhost:${PORT}`);
